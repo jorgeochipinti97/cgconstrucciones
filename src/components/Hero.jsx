26bdd8a9305b3fb43d7be4df2a362b42057089b1
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2023-10-21 19:03:04 -0300

    video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import dashboard from "../assets/images/dashboard.jpg";
 
 export const Hero = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-s
+
   return (
     <section
       className="w-screen  flex justify-center items-center bg-customDarkBg1 mb-[28vw] md:mb-[18vw] lg:mb-[10vw] xl:mb-[13vw] 2xl:mb-60 hero-bg-gradient pb-24 sm:pb-32 md:pb-44 lg:pb-0"
@@ -19,7 +19,7 @@ s
           transition={{ duration: 0.5 }}
         >
           <div className="text-customSecondary text-sm sm:text-base  mb-6 sm:mt-32 mt-16  font-bold">
-Construyendo sueños
+            Construyendo sueños
           </div>
         </motion.div>
         <motion.div
@@ -28,7 +28,7 @@ Construyendo sueños
           transition={{ duration: 0.5, delay: 0.05 }}
         >
           <div className="text-3xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-white  px-8 sm:px-8 md:px-20 lg:px-4">
-          GC Construcciones
+            GC Construcciones
           </div>
 
         </motion.div>
@@ -38,7 +38,7 @@ Construyendo sueños
           transition={{ duration: 0.5, delay: 0.1 }}
         >
           <div className="text-customGrayText text-sm lg:text-base xl:text-lg sm:text-base mt-10 px-12 sm:px-48  mb-20 sm:mb-40">
-Creando Realidad. CG, tu socio de confianza en construcción. Juntos, hacemos posibles tus proyectos
+            Creando Realidad. CG, tu socio de confianza en construcción. Juntos, hacemos posibles tus proyectos
           </div>
         </motion.div>
 
@@ -47,8 +47,8 @@ Creando Realidad. CG, tu socio de confianza en construcción. Juntos, hacemos po
           animate={{ opacity: 1, y: 0, zIndex: 20 }}
           transition={{ duration: 0.5, delay: 0.15 }}
         >
-          <div className="relative w-screen flex justify-center  md:mb-0 mb-10">
 
+          <div className=" mx-2 " style={{ border: '1px solid rgba(0,0,0,0.5)', WebkitBoxShadow: ' 5px 8px 65px 11px rgba(119,99,145,1)', MozBoxShadow: '5px 8px 65px 11px rgba(119,99,145,1)', boxShadow: '5px 8px 65px 11px rgba(119,99,145,1)', borderRadius: '50px' }}>
             <video src="/vide1.mp4" className="" playsInline controls muted loop autoPlay style={{ borderRadius: '50px' }} />
 
           </div>
